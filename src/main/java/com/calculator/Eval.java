4decf702d1c119f8eb3eee06557d4180b68974b6
arex123 <65856669+arex123@users.noreply.github.com>
2022-06-15 14:18:50 +0530

    Update Eval.java

--- src/main/java/com/calculator/Eval.java
+++ src/main/java/com/calculator/Eval.java
@@ -4,6 +4,91 @@ public class Eval {
     public static String evalExp(String exp){
         String ans="";
 
+        StringBuilder sb = new StringBuilder(exp);
+
+
+        //evaluting reminder
+        for(int i=0;i<sb.length();i++){
+
+            if(sb.charAt(i)=='%'){
+                float a =  (float)sb.charAt(i-1)-'0';
+                float b = (float)sb.charAt(i+1)-'0';
+
+                sb.delete(i-1,i+1);
+                float res = a%b;
+                sb.insert(i-1,res+"");
+
+            }
+
+        }
+
+
+        //evaluting division
+        for(int i=0;i<sb.length();i++){
+
+            if(sb.charAt(i)=='/'){
+                float a =  (float)sb.charAt(i-1)-'0';
+                float b = (float)sb.charAt(i+1)-'0';
+
+                sb.delete(i-1,i+1);
+                float res = a/b;
+                sb.insert(i-1,res+"");
+
+            }
+
+
+        }
+
+
+
+        //evaluting multiplication
+        for(int i=0;i<sb.length();i++){
+
+            if(sb.charAt(i)=='*'){
+                float a =  (float)sb.charAt(i-1)-'0';
+                float b = (float)sb.charAt(i+1)-'0';
+
+                sb.delete(i-1,i+1);
+                float res = a*b;
+                sb.insert(i-1,res+"");
+
+            }
+
+        }
+
+
+        //evaluting plus
+        for(int i=0;i<sb.length();i++){
+
+            if(sb.charAt(i)=='+'){
+                float a =  (float)sb.charAt(i-1)-'0';
+                float b = (float)sb.charAt(i+1)-'0';
+
+                sb.delete(i-1,i+1);
+                float res = a+b;
+                sb.insert(i-1,res+"");
+
+            }
+
+        }
+
+
+        //evaluting minus
+        for(int i=0;i<sb.length();i++){
+
+            if(sb.charAt(i)=='-'){
+                float a =  (float)sb.charAt(i-1)-'0';
+                float b = (float)sb.charAt(i+1)-'0';
+
+                sb.delete(i-1,i+1);
+                float res = a-b;
+                sb.insert(i-1,res+"");
+
+            }
+
+        }
+
+
 
 
 
